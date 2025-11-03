45fa6d99de58d94176a04fe273f2d85ebbe68f53
Yoav Ziv <yoav.ziv02@post.runi.ac.il>
2025-11-04 01:27:39 +0200

    cheers!

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,25 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	String word = args[0];
+        int num = Integer.parseInt(args[1]);
+        word.toUpperCase();
+
+        for(int i=0; i<word.length(); i++)
+        {
+                if(word.charAt(i) == 'A' || word.charAt(i) == 'E' || word.charAt(i) == 'F' || word.charAt(i) == 'H' || word.charAt(i) == 'I' || word.charAt(i) == 'L' || word.charAt(i) == 'M' || word.charAt(i) == 'N' || word.charAt(i) == 'O' || word.charAt(i) == 'R' || word.charAt(i) == 'S' || word.charAt(i) == 'X')
+                {
+                        System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                }
+                else
+                {
+                        System.out.println("Give me a " + word.charAt(i) + ": " + word.charAt(i) + "!");
+
+                }
+        }
+        System.out.println("What does that spell?");
+        for(int i=0; i<num; i++)
+        {
+                System.out.println(word + "!!!");
+        }
         }
 }
