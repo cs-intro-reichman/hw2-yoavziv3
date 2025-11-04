566137c09ab2a3145b7fead70c4c1b4528803d31
Yoav Ziv <yoav.ziv02@post.runi.ac.il>
2025-11-04 02:10:06 +0200

    hungry for pi?

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,27 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    double pi = Math.PI;
+		double total = 1;
+		int number = Integer.parseInt(args[0]);
+		Boolean flag = true;
+		double j = 3;
+
+		for(int i=0; i<number-1; i++)
+		{
+			if(flag == true)
+			{
+				total = total - (1/j);
+				flag = false;
+			}
+			else			
+			{
+				total = total + (1/j);
+				flag = true;
+			}
+			j= j + 2;
+		}
+		System.out.println("pi according to Java: " + pi);
+		System.out.println("pi, approximated: " + total*4);
 	}
 }
